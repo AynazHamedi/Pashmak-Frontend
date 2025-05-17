1a263428cb81440bf83155f883dc963d36455899
aria.vajarii <aria.vajarii@gmail.com>
2025-05-17 19:59:46 +0000

    feature: Bugfix/fix set cookie on localhost #0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs"; 
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    //to use when debuging:
+    // https: {
+    //   key: fs.readFileSync('./cert/develop.darkube.app-key.pem'),
+    //   cert: fs.readFileSync('./cert/develop.darkube.app.pem'),
+    // },
+    // host: "develop.darkube.app",
+    // allowedHosts: ["pashmak.darkube.app","develop.darkube.app"],
+
+    //to use when deploying
     host: "0.0.0.0",
-    port: 5173,
     allowedHosts: ["pashmak.darkube.app"],
+    
+    port: 5173,
     cors: true,
   },
 });
